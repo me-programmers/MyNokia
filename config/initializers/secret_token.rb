f7102f1de9a9c478debc8e77cd39d5fa65ffc03d
iamprogram <me.programmers@gmail.com>
2013-05-16 14:30:54 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyNokiaSolution::Application.config.secret_token = 'cd03e75d13af427918726a84a0c3443773eeaf8db1bf97ce5c449b5b418f33f75b9a7348a37ec7ee698460a4edc77defd73d2eb2a28ec1ac7af83dcbf9f9eb3b'
